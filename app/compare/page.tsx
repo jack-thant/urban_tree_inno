970af6ecd901ba7cd0462998f92f544e92c66fe6
jack-thant <thanthtoo5728@gmail.com>
2024-06-03 14:25:29 +0800

    Implement a control toggle for both heatspot and tree data in compare page

--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -6,13 +6,25 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { lightingEffect, INITIAL_VIEW_STATE } from "@/app/lib/mapconfig";
 import { InterpolatedTempRecord, TreePosition } from '../lib/definitions';
 import { useEffect, useState } from 'react';
-import {ScatterplotLayer} from '@deck.gl/layers';
-import {ScreenGridLayer} from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { ScreenGridLayer } from '@deck.gl/aggregation-layers';
+import { Switch } from "@/components/ui/switch"
+import Image from "next/image";
 
 export default function Compare() {
 
     const [treeData, setTreeData] = useState<Array<TreePosition>>([]);
     const [testData, setTestData] = useState(null);
+    const [toggleHeatSpot, setHeatSpot] = useState<boolean>(false);
+    const [toggleTree, setTreeToggle] = useState<boolean>(false);
+
+    const handleHeatSpotCheckedChange = (checked: boolean) => {
+        setHeatSpot(checked);
+    }
+
+    const handleTreeSpotCheckedChange = (checked: boolean) => {
+        setTreeToggle(checked);
+    }
 
     useEffect(() => {
         const fetchTreeData = async () => {
@@ -25,7 +37,7 @@ export default function Compare() {
             }
         }
         fetchTreeData();
-    },[]);
+    }, []);
 
     useEffect(() => {
         const fetchTempData = async () => {
@@ -38,31 +50,39 @@ export default function Compare() {
             }
         }
         fetchTempData();
-    },[]);
-    
-    console.log(testData);
+    }, []);
 
-    const layers = treeData && testData ? [
-        new ScatterplotLayer<TreePosition>({
-            id: 'scatter-plot',
-            data: treeData,
-            radiusScale: 10,
-            radiusMinPixels: 0.25,
-            getPosition: (d: TreePosition) => [d.lon, d.lat],
-            getFillColor: [22,163,74], 
-            getRadius: 1,
-        }),
-        new ScreenGridLayer<InterpolatedTempRecord>({
-            id: 'heat-map-grid',
-            data: testData,
-            opacity: 0.3,
-            getPosition: d => [d.lon, d.lat],
-            getWeight: d => d['Mean Temperature'],
-            cellSizePixels: 15,
-            aggregation: "MEAN",
-            colorRange: [[63, 127, 255], [92, 156, 255], [121, 182, 255], [255, 0, 0], [176, 48, 96], [198, 40, 40]],
-        })
-    ] : [];
+    const layers = [];
+
+    if (treeData && testData) {
+        if (toggleTree) {
+            layers.push(
+                new ScatterplotLayer<TreePosition>({
+                    id: 'scatter-plot',
+                    data: treeData,
+                    radiusScale: 10,
+                    radiusMinPixels: 0.25,
+                    getPosition: (d: TreePosition) => [d.lon, d.lat],
+                    getFillColor: [22, 163, 74],
+                    getRadius: 1,
+                })
+            );
+        }
+        if (toggleHeatSpot) {
+            layers.push(
+                new ScreenGridLayer<InterpolatedTempRecord>({
+                    id: 'heat-map-grid',
+                    data: testData,
+                    opacity: 0.3,
+                    getPosition: d => [d.lon, d.lat],
+                    getWeight: d => d['Mean Temperature'],
+                    cellSizePixels: 15,
+                    aggregation: "MEAN",
+                    colorRange: [[63, 127, 255], [92, 156, 255], [121, 182, 255], [255, 0, 0], [176, 48, 96], [198, 40, 40]],
+                })
+            )
+        }
+    }
 
 
     return (
@@ -81,6 +101,7 @@ export default function Compare() {
                             mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
                             mapStyle="mapbox://styles/mapbox/outdoors-v12"
                         />
+
                     </DeckGL>
                 </div>
                 <div style={{ height: '100vh', width: '50vw', position: 'relative' }}>
@@ -94,8 +115,39 @@ export default function Compare() {
                             mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
                             mapStyle="mapbox://styles/mapbox/outdoors-v12"
                         />
+                        {/* Card to toggle */}
+                        <div className="flex flex-col h-screen float-right px-3 py-4 md:px-2">
+                            <div className="bg-white min-w-[300px] rounded-lg text-blac px-6 py-4">
+                                <h2 className='font-bold text-lg mb-4'>Layer Visibility Control</h2>
+                                <div className="flex flex-row justify-between gap-y-5 mt-4">
+                                    <div className="inline-flex items-center">
+                                        <Image
+                                            src="./urban_key_data/sun.svg"
+                                            width={20}
+                                            height={20}
+                                            alt="Icon of Heat Spot Data"
+                                        />
+                                        <p className="font-semibold text-sm ml-3">Heat Spot Data</p>
+                                    </div>
+                                    <Switch id="heat_spot" onCheckedChange={handleHeatSpotCheckedChange} />
+                                </div>
+                                <div className="flex flex-row justify-between gap-y-5 mt-4">
+                                    <div className="inline-flex items-center">
+                                        <Image
+                                            src="./impact_assess/tree-notfill.svg"
+                                            width={20}
+                                            height={20}
+                                            alt="Icon of Tree Data"
+                                        />
+                                        <p className="font-semibold text-sm ml-3">Tree Data</p>
+                                    </div>
+                                    <Switch id="tree_spot" onCheckedChange={handleTreeSpotCheckedChange} />
+                                </div>
+                            </div>
+                        </div>
                     </DeckGL>
                 </div>
+
             </div>
 
 
